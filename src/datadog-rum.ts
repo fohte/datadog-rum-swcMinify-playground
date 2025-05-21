50e2d4aaf6562032543aef1ded770df7ed6c9c3b
Hayato Kawai <fohte.hk@gmail.com>
2025-05-21 18:53:19 +0900

    init datadog rum

---
+++ src/datadog-rum.ts
@@ -0,0 +1,29 @@
+// Necessary if using App Router to ensure this file runs on the client
+'use client'
+
+import { datadogRum } from '@datadog/browser-rum'
+
+datadogRum.init({
+  applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID || '',
+  clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN || '',
+
+  site: 'datadoghq.com',
+  service: 'datadog-rum-debug',
+  env: 'test',
+
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 100,
+
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+
+  defaultPrivacyLevel: 'mask',
+  enablePrivacyForActionName: true,
+})
+
+export function DatadogInit() {
+  // Render nothing - this component is only included so that the init code
+  // above will run client-side
+  return null
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { DatadogInit } from '../datadog-rum'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DatadogInit />
+      <Component {...pageProps} />
+    </>
+  )
 }
